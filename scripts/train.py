bb34f354ff520390f7c826588571c96b16268d40
hanneskolk <hanneskolk@gmail.com>
2026-04-18 17:51:00 +0300

    new training

--- scripts/train.py
+++ scripts/train.py
@@ -4,17 +4,25 @@ model = YOLO("yolo11s.pt")  # small model
 
 model.train(
     data="dataset/dataset.yml",
-    epochs=50,
+    epochs=100,
     imgsz=640,
     batch=16,
     device=0,   # Colab GPU
     workers=4,
 
     # HIGH IMPACT SETTINGS
+    hsv_s=0.7,
+    hsv_v=0.5,
+    degrees=10,
+    scale=0.7,
     cache=True,
     mosaic=1.0,
     mixup=0.2,
 
+    optimizer='auto',
+    lr0=0.01,
+    lrf=0.01,
+    warmup_epochs=3,
     # PRIORITY: LOW MISSED OBJECTS
     conf=0.15,
     iou=0.5
